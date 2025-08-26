57f28f2297acf0b55ddd4e54aa9f1bb2083d582c
Masud0109 <rahamanmasud49@gmail.com>
2025-08-26 21:55:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/finance-tracker/', // Use your actual repo name, all lowercase and no spaces
+  base: '/Finance-Tracker/', // Use your actual repo name, all lowercase and no spaces
 });
